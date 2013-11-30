dea1c913b0717c883fde91624a61933fa409d127
Apprentice <apprentice@dbc-32.local>
2013-11-30 15:27:55 -0800

    integrate visuals, update README

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -2,10 +2,12 @@ $(function(){
   $('#toggle_synth').on('click', function(){
     $('nav').show()
     $('body').css('background-color', '#fff')
+    stopVisuals()
   })
 
   $('#toggle_visual').on('click', function(){
     $('nav').hide()
     $('body').css('background-color', '#222')
+    playVisuals()
   })
 })
