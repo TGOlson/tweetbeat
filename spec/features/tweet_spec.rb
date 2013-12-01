608c15f4b4a30649f3fcc0b7f36072fa436cdb59
Marcus <apprentice@Marcus.local>
2013-11-30 20:08:20 -0800

    just where I'm at now with testing

--- spec/features/tweet_spec.rb
+++ spec/features/tweet_spec.rb
@@ -1,7 +1,3 @@
 require "spec_helper"
 
-feature 'visitor arrives at home page' do
-  scenario 'user clicks to start stream'
-    # click_link '#start-stream'
-    expect(page).to have_content('Tweet')
-end
+### Capybara Testing seems unnecessary at the moment
