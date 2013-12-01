278fc1c12a7eceaf54465ca788581b459c849f38
Marcus <apprentice@Marcus.local>
2013-11-30 18:52:03 -0800

    just updating commits before pulling down main branch

--- spec/features/tweet_spec.rb
+++ spec/features/tweet_spec.rb
@@ -1,1 +1,7 @@
 require "spec_helper"
+
+feature 'visitor arrives at home page' do
+  scenario 'user clicks to start stream'
+    # click_link '#start-stream'
+    expect(page).to have_content('Tweet')
+end
