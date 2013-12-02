3dc0b64a41815a382b9e7e3076bd89a5f76dcf23
Marcus <apprentice@Marcus.local>
2013-12-02 11:54:49 -0800

    making a slider

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -59,3 +59,17 @@ var Layout = {
     })
   }
 }
+
+  $(function() {
+    $( "#slider-vertical" ).slider({
+      orientation: "vertical",
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 60,
+      slide: function( event, ui ) {
+        $( "#amount" ).val( ui.value );
+      }
+    });
+    $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+  });
