efe4a7eba2a5e54601c801d8499d5350a309d672
Apprentice <apprentice@dbc-32.local>
2013-12-01 20:05:33 -0800

    create new element on drop

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -2,11 +2,8 @@ var Layout = {
   init: function(){
     $('#toggle_synth').on('click', this.toggleSynth)
     $('#toggle_visual').on('click', this.toggleVisual)
-    // $('.topic').draggable({ revert: "invalid" })
-    // this.setDropArea()
-    $( "#topics_list, #empty_synth_list" ).sortable({
-      connectWith: ".connected"
-    }).disableSelection();
+    $('.topic').draggable({ revert: "invalid" })
+    this.setDropArea()
   },
 
   toggleSynth: function(){
@@ -27,27 +24,15 @@ var Layout = {
       drop: function( event, ui ) {
 
         var keyword = ui.helper
-        console.log(keyword)
-        $(keyword).effect( "transfer", { to: this, className: "ui-effects-transfer" }, 300 )
-        $(this).html(keyword.textContent)
+        $(keyword).effect( "transfer", { to: this, className: "ui-effects-transfer" }, 300 ).fadeOut(100)
 
-        $(keyword).width( $(this).width() )
-        keyword.offset({
-          left: this.offsetLeft,
-          top: this.offsetTop + ( $(this).width() * .6 )
-        })
-
-        $(keyword).css('background-color', 'grey')
+        $(this).find('div').html(keyword.text())
+        $(this).find('div').addClass('keyword_dropped').hide().fadeIn()
 
         // var soundID = event.target.id
         // var keywordID = keyword[0].id
         // Stream.bindKeywordToSound(keywordID, soundID)
       }
     })
-  },
-
-  setPadSize: function(){
-    var width = $('#synth_pads').find('li').width()
-    $('#synth_pads').find('li').height(width)
   }
 }
