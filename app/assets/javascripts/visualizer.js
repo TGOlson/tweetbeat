cb29f9e2e37915fff068c5b75ea2431c3374cb4a
tgolson <tydotg@gmail.com>
2013-12-03 10:38:37 -0800

    d3 refactoring

--- app/assets/javascripts/visualizer.js
+++ app/assets/javascripts/visualizer.js
@@ -1,31 +1,42 @@
 var Visualizer = {
+
+  color: d3.scale.category20c(),
+  width: $(window).width() - 5,
+  height: $(window).height() - 5,
+
   start: function(){
-
-    var width = $(window).width() - 5
-    var height = $(window).height() - 5
-
-    var color = d3.scale.category20c()
-
-    var i = 0
-
-    var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
+    var svg = this.setSvgToBody()
 
     svg.append("rect")
-    .attr("width", width)
-    .attr("height", height)
-    .on("ontouchstart" in document ? "touchmove" : "click", particle)
+    .attr("width", Visualizer.width)
+    .attr("height", Visualizer.height)
+    .on("click", function(){ Visualizer.appendNewSvg(svg) })
+  },
 
-    function particle() {
+  setSvgToBody: function(width, height){
 
-      var m = d3.mouse(this)
+    var svg = d3.select("body").append("svg")
+    .attr("width", Visualizer.width)
+    .attr("height", Visualizer.height)
+    return svg
+  },
 
+  setEventHandlerToAppend: function(){
+
+  },
+
+  populate: function(){
+    $.each( $('.keyword_dropped'), function(i, e){
+      console.log(e)
+    })
+  },
+
+  appendNewSvg: function(svg){
       svg.insert("circle", "rect")
-      .attr("cx", m[0])
-      .attr("cy", m[1])
+      .attr("cx", 100)
+      .attr("cy", 200)
       .attr("r", 1e-6)
-      .style("stroke", color(++i))
+      .style("stroke", Visualizer.color(Math.floor( Math.random()*20 + 1 )))
       .style("stroke-opacity", 1)
       .transition()
       .duration(2000)
@@ -35,7 +46,6 @@ var Visualizer = {
       .remove()
 
       d3.event.preventDefault()
-    }
   },
 
   stop: function(){
