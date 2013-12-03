d30b9aef6eb634beb454b45479a7d9d893168025
Marcus <apprentice@Marcus.local>
2013-12-02 17:44:18 -0800

    Changed around the CSS so that the synth_container actually contains the pads and controls. Made the pads a little smaller so taht the XY pad could be in.
    We have a working volume slider as well.

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,3 +1,19 @@
+ $(function() {
+    $( "#slider-vertical" ).slider({
+      orientation: "vertical",
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 60,
+    })
+    $('#slider-vertical').slider({
+      change: function(event,ui) {
+        Layout.setVolume(ui.value) }
+    })
+  });
+
+
+
 var Layout = {
   init: function(){
     $('#toggle_synth').on('click', this.toggleSynth)
@@ -49,19 +65,15 @@ var Layout = {
         color: '#999'
       }, 200)
     })
+  },
+
+  setVolume: function(volume){
+    changeVolume(volume)
   }
 }
 
-  $(function() {
-    $( "#slider-vertical" ).slider({
-      orientation: "vertical",
-      range: "min",
-      min: 0,
-      max: 100,
-      value: 60,
-      slide: function( event, ui ) {
-        $( "#amount" ).val( ui.value );
-      }
-    });
-    $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
-  });
+
+
+
+
+
