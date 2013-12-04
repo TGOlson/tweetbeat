12548b01fa7cd1b096ac869b4945f50425f577f9
tgolson <tydotg@gmail.com>
2013-12-04 10:29:18 -0800

    d3 works with testing

--- spec/javascripts/visualizerSpec.js
+++ spec/javascripts/visualizerSpec.js
@@ -1,16 +1,10 @@
-// include external files like so:
-//= <reference path="http://d3js.org/d3.v3.min.js" />
 
-// describe('visualizer', function(){
-//   describe('start', function(){
-//     it('should set the svg canvas', function(){
-//       var d3Spy = jasmine.createSpyObj('d3', ['scale'])
-//       spyOn(d3, 'scale').andReturn(d3Spy)
-
-//       spyOn(Visualizer, 'setSvgCanvas')
-//       Visualizer.start()
-//       // d3 = null
-//       expect(Visualizer.setSvgCanvas).toHaveBeenCalled()
-//     })
-//   })
-// })
+describe('visualizer', function(){
+  describe('start', function(){
+    it('should set the svg canvas', function(){
+      spyOn(Visualizer, 'setSvgCanvas')
+      Visualizer.start()
+      expect(Visualizer.setSvgCanvas).toHaveBeenCalled()
+    })
+  })
+})
