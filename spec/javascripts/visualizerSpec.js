be02be1ce79aaf6571bb2011037337db18f64402
tgolson <tydotg@gmail.com>
2013-12-04 10:44:27 -0800

    test visualizer properties

--- spec/javascripts/visualizerSpec.js
+++ spec/javascripts/visualizerSpec.js
@@ -1,10 +1,34 @@
-
 describe('visualizer', function(){
-  describe('start', function(){
-    it('should set the svg canvas', function(){
-      spyOn(Visualizer, 'setSvgCanvas')
-      Visualizer.start()
-      expect(Visualizer.setSvgCanvas).toHaveBeenCalled()
+
+  describe('visualizer properties', function(){
+    it('should have a d3 color scale', function(){
+      expect(Visualizer.color).toBeDefined()
+      expect(Visualizer.color(1)).toEqual('#3182bd')
     })
+    it('should define width and height of the canvas', function(){
+      expect(Visualizer.width).toBeDefined()
+      expect(Visualizer.height).toBeDefined()
+    })
+    it('should have an svg element that defaults to null', function(){
+      expect(Visualizer.svg).toBeNull()
+    })
+  })
+
+  describe('visualizer methods', function(){
+
+    describe('start', function(){
+      it('should set the svg canvas', function(){
+        spyOn(Visualizer, 'setSvgCanvas')
+        Visualizer.start()
+        expect(Visualizer.setSvgCanvas).toHaveBeenCalled()
+      })
+      it('should populate svg canvas', function(){
+        spyOn(Visualizer, 'populate')
+        Visualizer.start()
+        expect(Visualizer.populate).toHaveBeenCalled()
+      })
+    })
+
+
   })
 })
