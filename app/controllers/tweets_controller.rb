0cfb90b52069a2df9c9263a1477127f5f4e92b7e
tgolson <tydotg@gmail.com>
2013-12-05 11:47:58 -0800

    tweet controller refactor

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,44 +1,33 @@
-require 'formater/sse'
+require 'formatter/sse'
 
 class TweetsController < ApplicationController
   include ActionController::Live
 
-  TOPICS = ["Coffee", "Tea", "DBCsleeps", "Canada", "USA",
-            "California", "Tesla", "Spring", "Summer", "Autumn",
-            "Winter", "Santa", "Snowman", "Moltar", "hurricane",
-            "tornado", "earthquake", "tsunami", "blizzard",
-            "Godzilla", "King Kong", "John Lennon", "Voltar",
-            "tapioca", "Star Wars", "Xolov"]
-
   def index
-    @topics = TOPICS
-    @bindings_and_names = [['q', 'SYNTH1'], ['w', 'SYNTH2'], ['e', 'SYNTH3'], ['a', 'BASS1'], ['s', 'BASS2'],
-                           ['d', 'BASS3'], ['z', 'KICK1'], ['x', 'KICK2'], ['c', 'PERC3']]
+    @topics = Topic.all
+    @bindings_and_names = Synthpad.all
   end
 
   def stream
     response.headers['Content-Type'] = 'text/event-stream'
-
     sse = Formatter::SSE.new(response.stream)
-
     client = TwitterClient.new.client
 
     begin
-      client.filter(:track => TOPICS.join(",")) do |tweet|
+      client.filter(:track => Topic.all.join(",")) do |tweet|
         text = tweet.text
-        TOPICS.each_with_index do |topic, index|
+        Topic.all.each_with_index do |topic, index|
           sse.write({ :content => text }, :event => index) if text.downcase.match(topic.downcase)
         end
       end
 
     rescue IOError
-
     ensure
       sse.close
     end
   end
 
   def topics
-    render json: TOPICS
+    render json: Topic.all
   end
 end
