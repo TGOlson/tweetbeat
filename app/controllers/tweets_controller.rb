85ad30b0430fa1eedd15eb3f920e3380bc8fb929
tgolson <tydotg@gmail.com>
2013-12-05 12:23:54 -0800

    set a concern with the stream to abstract from the controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,3 @@
-require 'formatter/sse'
-
 class TweetsController < ApplicationController
   include ActionController::Live
 
@@ -9,22 +7,7 @@ class TweetsController < ApplicationController
   end
 
   def stream
-    response.headers['Content-Type'] = 'text/event-stream'
-    sse = Formatter::SSE.new(response.stream)
-    client = TwitterClient.new.client
-
-    begin
-      client.filter(:track => Topic.all.join(",")) do |tweet|
-        text = tweet.text
-        Topic.all.each_with_index do |topic, index|
-          sse.write({ :content => text }, :event => index) if text.downcase.match(topic.downcase)
-        end
-      end
-
-    rescue IOError
-    ensure
-      sse.close
-    end
+    Stream.start(response)
   end
 
   def topics
