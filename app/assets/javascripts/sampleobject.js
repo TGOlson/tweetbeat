6bdc7fbbf0a26252839ac0043cb51721194205a4
Daniel Earthy <danielearthy@gmail.com>
2013-12-05 12:29:41 -0800

    Added Mario and HipHop sampleLib

--- app/assets/javascripts/sampleobject.js
+++ app/assets/javascripts/sampleobject.js
@@ -7,7 +7,15 @@ var sampleLibrary = [
 
 ["audio/Dminor_space_chord_root.mp3","audio/Dminor_space_chord_root+2.mp3","audio/Dminor_space_chord_root-2.mp3",
 "audio/Dminor_space_bass.mp3","audio/Dminor_space_bass_+2-1.mp3","audio/Dminor_space_bass_-2.mp3",
-"audio/Kick.mp3","audio/twinkle.mp3","audio/Airlock.mp3"]
+"audio/Kick.mp3","audio/twinkle.mp3","audio/Airlock.mp3"],
+
+["audio/hiphop/yo.mp3","audio/hiphop/nelly.mp3","audio/hiphop/piano.mp3",
+ "audio/hiphop/clap.mp3", "audio/hiphop/scratch1.mp3","audio/hiphop/scratch2.mp3",
+ "audio/hiphop/kick.mp3", "audio/hiphop/hat.mp3","audio/hiphop/snare.mp3"],
+ 
+["audio/mario/1up.mp3","audio/mario/Bowser.mp3","audio/mario/Coin.mp3",
+ "audio/mario/Kick-1.mp3", "audio/mario/Mario-dies.mp3", "audio/mario/Pipe.mp3",
+ "audio/mario/Power-appears.mp3","audio/mario/Power-up.mp3","audio/mario/Stomp.mp3"]
 
 ]
 var sampleBuffers = new Array ()
