7f64815e45acc6614c9d58935c1b776d620b1849
TGOlson <tydotg@gmail.com>
2014-02-08 18:33:43 -0800

    add firebase to server side

--- app/controllers/concerns/stream.rb
+++ app/controllers/concerns/stream.rb
@@ -6,11 +6,25 @@ module Stream
     response.headers['Content-Type'] = 'text/event-stream'
     sse = Formatter::SSE.new(response.stream)
 
+
+    base_uri = 'https://tweetbeatfb.firebaseio.com/'
+    secret_key = ENV['FIREBASE_SECRET']
+
+    firebase = Firebase.new(base_uri, secret_key)
+    count = 0
+
     begin
       client.filter(:track => Topic.all.join(",")) do |tweet|
         text = tweet.text
         Topic.all.each_with_index do |topic, index|
-          sse.write({ :content => text }, :event => index) if text.downcase.match(topic.downcase)
+          if text.downcase.match(topic.downcase)
+            firebase.push('Tweets', { topic: topic, text: text, index: index })
+            count +=1
+          end
+          if count == 10
+            firebase.delete('Tweets')
+            count = 0
+          end
         end
       end
 
