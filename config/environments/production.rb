27625eb0f35095e8d4926acaa8e29417f8de4f53
TGOlson <tydotg@gmail.com>
2014-02-10 12:39:20 -0800

    remove config.threadsafe - depreciated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,4 @@ TweetBeat::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  config.threadsafe!
 end
